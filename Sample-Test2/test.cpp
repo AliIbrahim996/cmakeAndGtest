ed3be86172483a4a35a5d5432c2cec746182440a
Ali Ibrahim <ali.ibrahim.2@lit-co.net>
2021-06-24 09:18:44 +0300

    clean up CMakeLists files

--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -12,26 +12,25 @@ void setter(int n, char* hamstersPosition) {
         else
             numberOfSittingHamster++;
     }
-}
-
-TEST(problemsolve424A14, test1) {
     n = 4;
     testHamstersPosition[0] = 'x';
     testHamstersPosition[1] = 'x';
     testHamstersPosition[2] = 'X';
     testHamstersPosition[3] = 'x';
-    setter(n, testHamstersPosition);
     hamstersPosition[0] = 'X';
     hamstersPosition[1] = 'x';
     hamstersPosition[2] = 'X';
     hamstersPosition[3] = 'x';
     minMinutes = 1;
+}
+
+TEST(problemsolve424A14, test1) {
+    setter(n, testHamstersPosition);
     s.solve424A14(n, numberOfStandingHamster, testMinMinutes, numberOfSittingHamster,
         testHamstersPosition);
     ASSERT_EQ(minMinutes, testMinMinutes);
-}
-;
-TEST(problemsolve424A14, test2){
+};
+TEST(problemsolve424A14, test2) {
     for (int i = 0; i < n; i++)
-        EXPECT_EQ(hamstersPosition[i], testHamstersPosition[i]); 
-}
+        EXPECT_EQ(hamstersPosition[i], testHamstersPosition[i]);
+};
