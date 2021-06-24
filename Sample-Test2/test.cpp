ffb6105c78823591f32ebf0f41f94a9a28ea6d2e
Ali Ibrahim <ali.ibrahim.2@lit-co.net>
2021-06-24 09:40:15 +0300

    fixing a bug in test.cpp

--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -1,33 +1,36 @@
 #include "sampleTest.h"
 Solver s;
-int n, numberOfStandingHamster = 0, minMinutes = 0, testMinMinutes = 0,
+int n, numberOfStandingHamster = 0, minMinutes = 0, testMinMinutes,
 numberOfSittingHamster = 0;
-char* hamstersPosition = new char[n];
-char* testHamstersPosition = new char[n];
+char* hamstersPosition;
+char* testHamstersPosition;
 
 void setter() {
-    for (int i = 0; i < n; i++) {
-        if (hamstersPosition[i] == 'X')
-            numberOfStandingHamster++;
-        else
-            numberOfSittingHamster++;
-    }
     n = 4;
+    hamstersPosition = new char[n];
+    testHamstersPosition = new char[n];
     testHamstersPosition[0] = 'x';
     testHamstersPosition[1] = 'x';
     testHamstersPosition[2] = 'X';
     testHamstersPosition[3] = 'x';
+   
+    for (int i = 0; i < n; i++) {
+        if (testHamstersPosition[i] == 'X')
+            numberOfStandingHamster++;
+        else
+            numberOfSittingHamster++;
+    }
     hamstersPosition[0] = 'X';
     hamstersPosition[1] = 'x';
     hamstersPosition[2] = 'X';
     hamstersPosition[3] = 'x';
     minMinutes = 1;
 }
-
 TEST(problemsolve424A14, test1) {
     setter();
     s.solve424A14(n, numberOfStandingHamster, testMinMinutes, numberOfSittingHamster,
         testHamstersPosition);
+    
     ASSERT_EQ(minMinutes, testMinMinutes);
 };
 TEST(problemsolve424A14, test2) {
